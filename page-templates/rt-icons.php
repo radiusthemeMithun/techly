20b24954722dad9d93a2611bb1d1c3cd98f38103
radiusthemeMithun <90020665+radiusthemeMithun@users.noreply.github.com>
2026-05-20 16:59:06 +0600

    Update

--- page-templates/rt-icons.php
+++ page-templates/rt-icons.php
@@ -1,4 +1,7 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 /**
  * Template Name: RT Icons
  *
@@ -11,6 +14,7 @@ get_header(); ?>
 	<div class="container">
 		<div class="row pt-50 pb-50 d-flex gap-15">
 			<?php
+			// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
 			echo techly_get_svg( 'search' );
 			echo techly_get_svg( 'facebook' );
 			echo techly_get_svg( 'twitter' );
@@ -22,6 +26,7 @@ get_header(); ?>
 			echo techly_get_svg( 'snapchat' );
 			echo techly_get_svg( 'whatsapp' );
 			echo techly_get_svg( 'reddit' );
+			// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 			?>
 		</div>
 	</div>
